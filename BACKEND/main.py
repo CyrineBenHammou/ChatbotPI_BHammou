2f9d290d1fa8f913afdd5ee659fe8b58b7fc2585
CyrineBenHammou <cyrine.bhammou@outlook.com>
2023-12-29 22:50:36 +0100

    V3

--- BACKEND/main.py
+++ BACKEND/main.py
@@ -2,8 +2,12 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 import uvicorn
 from fastapi.middleware.cors import CORSMiddleware
-import nltk
 from fastapi.staticfiles import StaticFiles
+#from textblob import TextBlob
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+import string
 
 # Create a FastAPI instance
 app = FastAPI()
@@ -19,8 +23,9 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-def analyse_text(text:str):
-    return {"sujet":"vide","sentiment":[],"mot_clés":[]}
+def analyse_text(texte:str):
+    mot_cle=nltk.word_tokenize(texte)
+    return {"sujet":"vide","sentiment":[],"mot_cles":mot_cle}
 
 def generer_reponse(text:str):
     return {"reponse":"reponse"}
@@ -28,20 +33,48 @@ def generer_reponse(text:str):
 def formeter_reponse(text:str):
     return {"reponse_formater":"reponse vide formater"}
 
+
 # Define a Pydantic model for the input data
 class AnalyseTextInput(BaseModel):
-    text: str
+    texte: str
 
 # Define a POST endpoint at the path "/analyse"
 @app.post("/analyse")
 def analyse_endpoint(analyse_input: AnalyseTextInput):
-    # This function receives data from the client, validates it using Pydantic, and processes it
+
     print(analyse_input)
-    return {"msg":analyse_input}
+    #miniscule
+    texte=(analyse_input.texte).lower()
+    #ponctuation
+    texte = ' '.join([char for char in texte if char not in string.punctuation])
+    #texte.translate(str.maketrans("", "", string.punctuation))
+
+    #erreur:Faute d'orthographe
+    """blob = TextBlob(texte)
+    texte = blob.correct()
+    print(texte.words)
+    print(type(texte.words))"""
+
+    #tokenisation
+    tokens=nltk.word_tokenize(texte)
+    print(tokens)
+
+    #stopwords
+    stop_words = set(stopwords.words('english'))
+    tokens = [word for word in tokens if word not in stop_words]
+    print(tokens)
+
+    #Stemmer & Lemmatization
+    
+    #porter = PorterStemmer()
+    lemmatizer = WordNetLemmatizer()
+    
+    #stemmed_words = [porter.stem(word) for word in tokens]
+    lemmatized_words = [lemmatizer.lemmatize(word) for word in tokens]
+    print(lemmatized_words)
+
+    return {"msg": analyse_input}
 
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000)
 
-# uvicorn main:app  --reload --host 0.0.0.0 --port 8000
-
-# ASSSLEEMMAAAA
--- FRONTEND/js/app.js
+++ FRONTEND/js/app.js
@@ -1,33 +1,53 @@
-var BTN=document.querySelector("button")
-var TEXTAREA=document.querySelector("textarea")
-var DIV=document.querySelector("#response_msg")
 
-//EVENT
-BTN.addEventListener("click", ChatBot)
+  var BTN = document.querySelector("button");
+  var TEXTAREA = document.querySelector("#textSpeech");
+  var DIV = document.querySelector("#response_msg");
+  var BTN_MIC = document.querySelector("#bMic");
 
+  var recognition = new webkitSpeechRecognition();
+  recognition.continuous = false;
+  recognition.lang = 'en-US';
+  recognition.interimResults = false;
+  recognition.maxAlternatives = 1;
 
-//Main function
+  // Event listeners
+  BTN.addEventListener("click", chatBot);
+  BTN_MIC.addEventListener("click", speechToText);
 
-function ChatBot(){
+  // Function to send text from TEXTAREA to the backend for analysis
+  function chatBot() {
+    let text = TEXTAREA.value;
+    var url_backend = "http://127.0.0.1:8000/analyse"
 
-    console.log("Button clicked")
-    let text=TEXTAREA.value
-    var myHeaders = new Headers();
-    myHeaders.append("Content-Type", "application/json");
-    
-    var raw = JSON.stringify({
-      "text": text
-    });
-    
-    var requestOptions = {
-      method: 'POST',
-      headers: myHeaders,
-      body: raw,
-      // redirect: 'follow'
-    };
-    
-    fetch("http://127.0.0.1:8000/analyse", requestOptions)
-      .then(response => response.text())
-      .then(result => console.log(result))
-      .catch(error => console.log('error', error));
-}
+    fetch(url_backend, {
+      method: "POST",
+      body: JSON.stringify({"texte": text}),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(response => {
+        response.json()
+          .then(data => {
+            console.log(data);
+          });
+      })
+      .catch(error => {
+        console.warn(error);
+      });
+  }
+
+  // Function to start speech-to-text functionality
+  function speechToText() {
+    alert("Speech to text ready");
+    recognition.start();
+  }
+
+  // Event handler for speech recognition results
+  recognition.onresult = function(event) {
+    var message = event.results[0][0].transcript;
+    console.log('Result received: ' + message + '.');
+    console.log('Confidence: ' + event.results[0][0].confidence);
+    TEXTAREA.value = message;
+  };
+
